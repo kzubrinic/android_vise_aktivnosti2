9cadcd007d857b37805e81a88a53d27a7a2b7b03
Krunoslav Zubrinic <kruno.zubrinic@gmail.com>
2019-03-03 16:55:51 +0100

    Prvi commit

---
+++ src/main/java/hr/kruno/viseaktivnosti/NistaNeVracaActivity.java
@@ -0,0 +1,22 @@
+package hr.kruno.viseaktivnosti;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class NistaNeVracaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Aktivnost koja ništa ne vraća pozivatelju");
+        setContentView(R.layout.activity_nista_ne_vraca);
+        // Dohvat parametara od strane pozivatelja
+        Intent intent = getIntent();
+        // Parametar je niz znakova
+        String poruka = intent.getStringExtra("poruka");
+        TextView tv = (TextView)findViewById(R.id.tv);
+        tv.setText(poruka);
+    }
+}
---
+++ src/main/java/hr/kruno/viseaktivnosti/VracaPozivateljuActivity.java
@@ -0,0 +1,35 @@
+package hr.kruno.viseaktivnosti;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class VracaPozivateljuActivity extends AppCompatActivity {
+    private Intent i;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Druga aktivnost");
+        setContentView(R.layout.activity_vraca_pozivatelju);
+        i = getIntent();
+        // Parametar je niz znakova
+        String poruka = i.getStringExtra("poruka");
+        TextView dobiven = (TextView)findViewById(R.id.dobiven);
+        dobiven.setText(poruka);
+    }
+
+    public void vratiNatrag(View v){
+        // Pozvat će aktivnost koja je upravljana klasom NistaneVraca
+        EditText ed = (EditText)findViewById(R.id.ed);
+        // Pozivatelju se povratna vrijednost vraća unutar parametara onog intenta
+        //   koji je pozivateljj poslao ovoj aktivnosti (nadopunjavaju se parametri).
+        i.putExtra("povratnaPoruka", ed.getText().toString());
+        // Sve je Ok
+        setResult(RESULT_OK, i);
+        // završetak - potvrda
+        finish();
+    }
+}
