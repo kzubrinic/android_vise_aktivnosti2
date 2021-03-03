c380d13e0e4877d34fea91d10bc439667040b4e6
Kruno Zubrinic <kruno.zubrinic@gmail.com>
2021-03-03 19:59:42 +0100

    Dorada aplikacije - dodana još jedna aktivnost koja vraća rezultat pozivatelju.

--- app/src/main/java/hr/kruno/viseaktivnosti/MainActivity.java
+++ app/src/main/java/hr/kruno/viseaktivnosti/MainActivity.java
@@ -1,5 +1,6 @@
 package hr.kruno.viseaktivnosti;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,8 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private final int OK_CODE = 1;
+    private final int ID_AKTIVNOSTI = 1;
+    private final int ID_DRUGE_AKTIVNOSTI = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,22 +18,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void pozoviAktivnost(View v){
-        // Pozvat će aktivnost koja je upravljana klasom NistaneVraca
+        // Pozvat će aktivnost koja je upravljana klasom NistaneVracaActivity
         Intent i = new Intent(this, NistaNeVracaActivity.class);
         // Šalje se 1 parametar - String s ključem "poruka" i vrijednošću "Dobar dan!"
         // Ako se drugoj aktivnosti ne šalju parametri, ovaj redak nije potreban
-        i.putExtra("poruka","Dobar dan!");
+        i.putExtra("poruka","Dobra večer!");
         // Poziv aktivnosti koja ništa ne vraća pozivatelju
         startActivity(i);
     }
     public void pozoviAktivnostKojaVraca(View v){
-        // Pozvat će aktivnost koja je upravljana klasom NistaneVraca
+        // Pozvat će aktivnost koja je upravljana klasom VracaPozivateljuActivity
         Intent i = new Intent(this, VracaPozivateljuActivity.class);
         // Šalje se 1 parametar - String s ključem "poruka" i vrijednošću "Dobar dan!"
         // Ako se drugoj aktivnosti ne šalju parametri, ovaj redak nije potreban
         i.putExtra("poruka","Dobar dan!");
         // Poziv aktivnosti koja vraća rezultat pozivatelju
-        startActivityForResult(i, OK_CODE);
+        startActivityForResult(i, ID_AKTIVNOSTI);
+    }
+    public void pozoviAktivnostKojaVracaDruga(View v){
+        // Pozvat će aktivnost koja je upravljana klasom VracaPozivateljuDrugaActivity
+        Intent i = new Intent(this, VracaPozivateljuDrugaActivity.class);
+        // Poziv aktivnosti koja vraća rezultat pozivatelju
+        startActivityForResult(i, ID_DRUGE_AKTIVNOSTI);
     }
 
     // Ova metoda se automatski izvodi nakon što pozvana aktivnost završi izvođenje
@@ -41,12 +49,20 @@ public class MainActivity extends AppCompatActivity {
 
         // Provjerava je li druga aktivnost uspješno završila
         // Služi za identifikaciju određene aktivnosti
-        if (requestCode == OK_CODE) {
-            if (resultCode == RESULT_OK) {
-                // dohvaća podatke kojeje u poslani objekt tipa Intent dodala pozvana aktivnost
+        if (requestCode == ID_AKTIVNOSTI) {
+            if (resultCode == Activity.RESULT_OK) {
+                // dohvaća podatke koje je u poslani objekt tipa Intent dodala pozvana aktivnost
                 String povratna = data.getStringExtra("povratnaPoruka");
                 TextView vracen = findViewById(R.id.vracen);
                 vracen.setText(povratna);
+            }
+        } else if (requestCode == ID_DRUGE_AKTIVNOSTI) {
+            if (resultCode == Activity.RESULT_OK) {
+                // dohvaća podatke koje je u poslani objekt tipa Intent dodala pozvana aktivnost
+                boolean povratna = data.getBooleanExtra("povratnaPoruka", false);
+                TextView vracen = findViewById(R.id.vracenDrugi);
+                String novi = (povratna) ? "ISTINA":"LAŽ";
+                vracen.setText(novi);
             }
         }
     }
--- app/src/main/java/hr/kruno/viseaktivnosti/VracaPozivateljuDrugaActivity.java
+++ app/src/main/java/hr/kruno/viseaktivnosti/VracaPozivateljuDrugaActivity.java
@@ -3,33 +3,24 @@ package hr.kruno.viseaktivnosti;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Switch;
 
-public class VracaPozivateljuActivity extends AppCompatActivity {
-    private Intent i;
+public class VracaPozivateljuDrugaActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Druga aktivnost");
-        setContentView(R.layout.activity_vraca_pozivatelju);
-        i = getIntent();
-        // Parametar je niz znakova
-        String poruka = i.getStringExtra("poruka");
-        TextView dobiven = (TextView)findViewById(R.id.dobiven);
-        dobiven.setText(poruka);
+        setContentView(R.layout.activity_vraca_pozivatelju_druga);
     }
-
     public void vratiNatrag(View v){
         // Pozvat će aktivnost koja je upravljana klasom NistaneVraca
-        EditText ed = findViewById(R.id.ed);
+        Switch s = findViewById(R.id.switch1);
         // Pozivatelju se povratna vrijednost vraća unutar parametara onog intenta
         //   koji je pozivateljj poslao ovoj aktivnosti (nadopunjavaju se parametri).
         Intent i = new Intent();
-        i.putExtra("povratnaPoruka", ed.getText().toString());
+        i.putExtra("povratnaPoruka", s.isChecked());
         // Sve je Ok
         setResult(Activity.RESULT_OK, i);
         // završetak - potvrda
